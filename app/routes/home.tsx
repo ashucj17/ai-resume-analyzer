5f004cd224cf5f2f29493b8353ba9e3c610eb71c
Ashish Kumar <agnihotri.ashishkumar25@gmail.com>
2025-11-26 10:12:38 +0530

    ResumeCard

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,37 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "../../constants/index";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+  <Navbar />
+
+  <section className="main-section">
+    <div className="page-heading py-16 text-center">
+      <h1 className="text-4xl font-bold">Track Job Applications & Resume Assessment</h1>
+      <h2 className="text-lg text-gray-700 mt-2">
+        Evaluate your submission with automated AI feedback.
+      </h2>
+    </div>
+
+    {resumes.length > 0 && (
+      <div className="resumes-section grid gap-6 md:grid-cols-2 lg:grid-cols-3 px-4 pb-10">
+        {resumes.map((resume) => (
+          <ResumeCard key={resume.id} resume={resume} />
+        ))}
+      </div>
+    )}
+  </section>
+</main>
+
+  );
 }
